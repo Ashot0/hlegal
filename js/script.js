f8273feaf1a35f28b4358e188c4f9a270b9116a4
Ashot0 <illia.holovan@gmail.com>
2023-10-11 16:15:16 +0300

    burger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-// Строгий режим
 "use strict";
+const burger = document.getElementById("burger")
+const menu = document.getElementById('menu')
+let burgerVis = 0;
 
+function burgerm() {
+	if (burgerVis == 0) {
+		menu.style = "display:block";
+		burgerVis = 1;
+	} else {
+		menu.style = "display:none";
+		burgerVis = 0;
+	}
+}
